3ecb7cb00af6364ad57a4195c50aabe966084569
Lennox Kabo <borelennox@gmail.com>
2024-02-11 10:51:25 +0300

    pos first commit

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ForgotPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset emails and
+    | includes a trait which assists in sending these notifications from
+    | your application to your users. Feel free to explore this trait.
+    |
+    */
+
+    use SendsPasswordResetEmails;
+	// This method handles the form submission for sending the password reset email
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        $response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+                    ? back()->with('status', trans($response))
+                    : back()->withErrors(['email' => trans($response)]);
+    }
+}
